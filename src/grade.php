bedc5986edcc3bbb4e530cba6f9c5eb941480871
KOOLAPAT <koolapat@KOOLAPATs-MacBook-Pro.local>
2014-08-23 12:00:24 +0700

    Add all file both test and src

---
+++ src/grade.php
@@ -0,0 +1,28 @@
+<?php
+/**
+*  
+*/
+class Grade 
+{
+	public function tad($score){
+		if($score >= 80){
+			return "A";	
+		}else if($score >= 70){
+			return "B";
+		}else if($score >= 60){
+			return "C";
+		}else if($score >= 50){
+			return "D";
+		}else{
+			return "F";
+		}
+	}
+
+	
+
+
+	
+	
+}
+
+?>
---
+++ test/grade_test.php
@@ -0,0 +1,31 @@
+<?php
+#include "src/grade.php";
+
+class GradeUnitTest extends PHPUnit_Framework_TestCase{
+	function test_socre_80_get_A(){
+		$grade = new Grade();
+		$this->assertEquals("A",$grade->tad(80));
+	}
+
+	function test_socre_70_get_B(){
+		$grade = new Grade();
+		$this->assertEquals("B",$grade->tad(75));
+	}
+	function test_socre_60_get_C(){
+		$grade = new Grade();
+		$this->assertEquals("C",$grade->tad(60));
+	}
+	function test_socre_50_get_D(){
+		$grade = new Grade();
+		$this->assertEquals("D",$grade->tad(52));
+	}
+	function test_socre_0_get_F(){
+		$grade = new Grade();
+		$this->assertEquals("F",$grade->tad(49));
+	}
+	
+
+}
+
+
+?>
